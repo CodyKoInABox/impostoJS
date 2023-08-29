330dfeb9b7cb0a10cdcdc00a11cfd19ca46f568b
Cody Ko In A Box <codykoinabox@gmail.com>
2023-08-28 21:14:11 -0300

    Update imposto.js

--- imposto.js
+++ imposto.js
@@ -8,20 +8,6 @@ class Funcionario{
     }
 }
 
-function calcular(nome, salario){
-    let taxa = 11;
-
-    if(salario > 2500){
-        taxa = taxa + 8
-    }else{
-        taxa = taxa + 5
-    }
-
-    let liquido = salario * ((100-taxa) / 100)
-
-    return new Funcionario(nome, salario, taxa, liquido)
-}
-
 module.exports = function calcular(nome, salario){
     let taxa = 11;
 
